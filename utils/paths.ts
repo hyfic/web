76d6db80be6b20546544b988562a742b48df4724
fadhilsaheer <fadhilsaheer@gmail.com>
2022-03-20 10:17:22 +0530

    routes

---
+++ utils/paths.ts
@@ -0,0 +1,8 @@
+export enum Paths {
+  register = '/account/register',
+  login = '/account/login',
+  app = '/',
+  settings = '/account/settings',
+  terms = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+  notFound = '/404',
+}

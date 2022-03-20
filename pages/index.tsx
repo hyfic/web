ded2145b39fb3e7df90fa732f86a9cd4303183b5
fadhilsaheer <fadhilsaheer@gmail.com>
2022-03-20 09:08:01 +0530

    tailwind setup

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,11 @@
 import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
-  return <div>Hello World</div>;
+  return (
+    <div>
+      <h1 className='text-2xl'>Hello World</h1>
+    </div>
+  );
 };
 
 export default Home;
